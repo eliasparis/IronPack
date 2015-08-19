c31bd8c6cf632d32497e969620dacad494f4ecb0
Eli <55drk1@gmail.com>
2015-08-19 09:21:42 +0200

    index site and game routes finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,21 +3,54 @@ class UsersController < ApplicationController
 	before_action :require_login, except: :no_cookies
  
 	def index
-		render 'index'
+		unless current_user.screen === 0
+			screen_controller
+		else
+			render 'index'
+		end
+		
 	end
 
-	def play1
+	def game_one
+		unless current_user.screen === 1
+			screen_controller
+		else
+			render 'play1'
+		end
+		
+	end
+
+	def game_two
+		unless current_user.screen === 2
+			screen_controller
+		else
+			render 'play2'
+		end
+		
+	end
+
+	def wins_update_screen
 		@user = User.find_by(id: cookies[:user_id])
-		@user.screen = 1
+		@user.screen += 1
 		@user.save
-		render 'play1'
+		screen_controller
 	end
 
-	def no_cookies
+  	def no_cookies
 		render 'intro'
 	end
 
 	private
+
+	def screen_controller
+  		if current_user.screen === 1
+  			redirect_to play1_path
+  		elsif current_user.screen === 2
+  			redirect_to play2_path
+  		elsif current_user.screen === 0
+  			render 'index'
+  		end
+  	end
  
   	def require_login
 	    unless cookies[:user_id]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   get '/' => 'users#no_cookies', :as => 'intro'
   get '/player' => 'users#index'
-  get '/thegame' => 'users#play1', :as => 'play1'
+  get '/screen_one' => 'users#game_one', :as => 'play1'
+  get '/screen_two' => 'users#game_two', :as => 'play2'
+  get '/saving' => 'users#wins_update_screen', :as => 'save' 
   get '/:promocode' => 'sessions#create'
   post '/introdone' => 'sessions#create'
 
