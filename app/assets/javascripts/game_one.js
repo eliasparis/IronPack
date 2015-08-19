c92bf249deea2f4b8780ff63c9d48ad823403c14
Eli <55drk1@gmail.com>
2015-08-19 20:55:59 +0200

    First game done

--- app/assets/javascripts/game_one.js
+++ app/assets/javascripts/game_one.js
@@ -4,14 +4,83 @@ $(document).ready(function(){
 	$('#play_one_button').on('click', beginGameOne);
 
 	var instructions_game_one_html = 
-			['<div>ffffffffff',
+			['<div class="rainbow_sq">',
 			'</div>'
 		    ].join('\n');
 
-	function beginGameOne(event){
+	function beginGameOne(){
+		$('.one_ins_background').html(instructions_game_one_html);
+		rainbow_sq_clicks = 1;
+		w = 90;
+		h = 80;
+	}
+
+	var rainbow_sq_clicks = 1;
+	var w = 90;
+	var h = 80;
+
+	var save = "<span>SAVE GAME <br> AND PLAY NEXT</span>"
+
+	var cool = ['<div class="cool">Cool!',
+				'</div>',
+				'<div class="save">'+ save +'</div>'
+			    ].join('\n');
+
+	var nop = ['<div class="cool">NOP',
+			'</div>'
+		    ].join('\n');
+
+	$('.one_ins_background').on('click', function(e) { 
+   		if( e.target != this ) {
+   		}else{
+   			rainbowRepeat(event);
+   		};
+       	return;
+	});
+
+	$(document).on('click','.rainbow_sq', rainbowPlaying);
+
+	function rainbowRepeat (event){
 		event.preventDefault();
-		console.log('aupa');
-		$('.one_ins_background').text(instructions_game_one_html);
+			$('.one_ins_background').html(nop);
+			setTimeout(beginGameOne, 1000);
+	}
+
+	function rainbowPlaying (event){
+		event.preventDefault();
+
+		if (rainbow_sq_clicks < 4) {
+			w -= 16;
+			h -= 11;
+		}else if (rainbow_sq_clicks < 7){
+			w -= 9;
+			h -= 6;
+		}else if (rainbow_sq_clicks > 13){
+			$('.one_ins_background').html(cool);
+		}else{
+			w -=2;
+			h -=1;
+		};
+
+		rainbow_sq_clicks += 1;
+
+		var left = Math.floor(Math.random() * (90 - 5 + 1)) + 5; 
+		var top = Math.floor(Math.random() * (90 - 5 + 1)) + 5;
+		var deg = Math.floor(Math.random() * (360 - 10 + 1)) + 10;
+
+		$('.rainbow_sq').css('width', w+'%');
+		$('.rainbow_sq').css('height', h+'%');
+		$('.rainbow_sq').css('left', left+'%');
+		$('.rainbow_sq').css('top', top+'%');
+		$('.rainbow_sq').css('transform','rotate('+ top +'deg)' );
+		$('.rainbow_sq').css('-webkit-transform','rotate('+ top +'deg)' );
+		$('.rainbow_sq').css('-ms-transform','rotate('+ top +'deg)' );
+	}
+
+	$(document).on('click','.save',saveGame);
+
+	function saveGame(){
+		window.location.replace('/saving');
 	}
 
 })
